11552e79b60b8fb1aa396eba044caaa61b6952dd
Marcos Vitor <mv.piesske@gmail.com>
2020-06-18 18:07:40 -0300

    Implementing SplashScreen and fixing route's name

--- pages/Home.js
+++ pages/Home.js
@@ -1,18 +1,20 @@
+
 import React, {Component} from 'react';
-import { AppRegistry, StyleSheet, Text, View, Button, Alert } from 'react-native';
-  export default class Home extends React.Component{
-      render(){
+import {View, Text, Button} from 'react-native';
+
+export default class Home extends React.Component{
+    render(){
         return (
-            <View style={{flex: 1, justifyContent: 'center'}}>
-                <Text style={{fontSize: 30}}>Home Page guys :D</Text>
-                <Button title='To another page =>'
-                onPress = {() => this.props.navigation.navigate("Test")}
+            <View style={{flex: 1, justifyContent: "center"}}>
+                <Text style={{fontSize: 30}}>Hello World</Text>
+                <Button title="<= Back to Splash" 
+                onPress = {() => this.props.navigation.navigate("Splash")}
                 ></Button>
-                <Button title='Go to truth =>'
-                onPress = {() => this.props.navigation.navigate("Test2")}
+                <Text style={{height: 30}}> </Text>
+                <Button title="Go to Truth=>"
+                onPress ={() => this.props.navigation.navigate("Truth")}
                 ></Button>
             </View>
         );
     }
 }
-  
--- pages/Truth.js
+++ pages/Truth.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {View, Text, Button} from 'react-native';
 
-export default class Test2 extends React.Component{
+export default class Truth extends React.Component{
     render(){
         return(
-            <View>
+            <View style={{justifyContent: "center"}}>
                 <Text style={{fontSize: 30}}>BK > MCDonalds</Text>
                 <Button title="<= Back Home"
                 onPress = {() => this.props.navigation.navigate("Home")}
