8a025ebf5fc38e41181e4a8df21f3413fabca369
Marcos Vitor <mv.piesske@gmail.com>
2020-06-20 23:39:25 -0300

    Add new flash cards with TouchableOpacity, and doing new styles in styles.js

--- pages/Truth.js
+++ pages/Truth.js
@@ -1,36 +1,20 @@
 import React, {Component} from 'react';
-import Style from '../assets/styles'
 import {View, Text, Button, StyleSheet, TouchableOpacity,StatusBar} from 'react-native';
+import mainStyle from '../assets/styles';
 
 const txtTruth = "BK > McDonalds"
 
 export default class Truth extends React.Component{
     render(){
         return(
-            <View style={Style.container}>
+            <View style={mainStyle.container}>
                 <StatusBar  barStyle="light-content" backgroundColor="transparent" translucent={true} />
-                <Text style={{fontSize: 30}}>{txtTruth}</Text>
-                <TouchableOpacity style = {styles.button}
+                <Text style={mainStyle.txtDescription}>{txtTruth}</Text>
+                <TouchableOpacity style = {mainStyle.button}
                 onPress = {() => this.props.navigation.navigate("Home")}>
-                    <Text style={styles.txtButton}>Back Home</Text> 
+                    <Text style={mainStyle.txtButton}>Back Home</Text> 
                 </TouchableOpacity>
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    button: {
-        width: 185,
-        alignItems: "center",
-        backgroundColor: "#ff5725",
-        padding: 10,
-        fontSize: 22,
-        color: "#3f2e29",
-        borderRadius: 10,
-    },
-    txtButton: {
-        fontWeight: "bold",
-        color: "#f3f9f2"
-    }
-});
