4740df4d8e20017ec212f63887b3e521b204e437
Marcos Vitor <mv.piesske@gmail.com>
2020-06-27 15:17:05 -0300

    Implementing the list-function in Home

--- assets/scripts/Calcs.js
+++
@@ -1,4 +0,0 @@
-export const calcList = [
-    {id: 1, image: require('../../assets/img/save_image.png'), name: 'sameName1', description: 'description1'},
-    {id: 2, image: require('../../assets/img/save_image.png'), name: 'sameName2', description: 'description2'}
-]
--- assets/scripts/card.js
+++ assets/scripts/card.js
@@ -1,18 +1,17 @@
-import React, {Component, useState} from 'react';
+import React from 'react';
 import {View, Text, Image} from 'react-native';
 import mainStyle from '../scripts/styles';
-import {calcList} from '../scripts/Calcs';
 
-export default function CardHome(){
-    return(
-        calcList.map(f => (
-            <View key={f.id} style={mainStyle.card}>
+export default class CardHome extends React.Component{
+    render(){
+        return(
+            <View style={mainStyle.card}>
                 <View styles={mainStyle.cardContent}>
-                    <Image style={mainStyle.imgCard} source ={f.image} />
-                    <Text style={{color: '#f31203', fontSize: 15, marginTop: -25}}>{f.name}</Text>
-                    <Text style={{color: '#191b1c', fontSize: 12}}>{f.description}</Text>
+                    <Image style={mainStyle.imgCard} source={this.props.image} />
+                    <Text style={{color: '#f31203', fontSize: 15, marginTop: -25}}>{this.props.name}</Text>
+                    <Text style={{color: '#191b1c', fontSize: 12}}>{this.props.description}</Text>
                 </View>
             </View>
-         ))
-    )
+        )
+    }
 }
